447fe9b52ec479a57ac027a34d0c32cd878d2dda
CJH <CJH@172.17.2.78>
2015-05-14 10:29:08 +0900

    실습과제 9

---
+++ JavaPractice/src/org/dimigo/oop/Score.java
@@ -0,0 +1,41 @@
+package org.dimigo.oop;
+
+import java.util.Scanner;
+
+public class Score {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("국어 점수 입력 => ");
+		int kor=scanner.nextInt();
+		System.out.print("수학 점수 입력 => ");
+		int mat=scanner.nextInt();
+		System.out.print("영어 점수 입력 => ");
+		int eng=scanner.nextInt();
+		
+		StringBuilder sb = new StringBuilder()
+			.append("<<점수 출력>>\n")
+			.append("\n국어 점수 : ")
+			.append(kor)
+			.append("점\n")
+			.append("수학 점수 : ")
+			.append(mat)
+			.append("점\n")
+			.append("영어 점수 : ")
+			.append(eng)
+			.append("점\n")
+			.append("총점 : ")
+			.append(kor+mat+eng)
+			.append("점\n")
+			.append("평균 : ")
+			.append(String.format("%.1f",(kor+mat+eng)/3.0))
+			.append("점\n");
+		System.out.println(sb);
+		
+		scanner.close();
+	}
+
+}
